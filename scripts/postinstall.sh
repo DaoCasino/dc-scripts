7524e55937e724310ec42cb5ca05715c2d5f9dbf
Ilya fanyShu <ilyashvy@gmail.com>
2018-09-04 14:10:51 +0300

    refactor postinstall script and add npc module

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,10 +1,13 @@
+clear
 echo 'Contract generate'
-cd ./_env
+cd `dirname "$0"`/../_env/
 
 echo 'Docker up'
 docker-compose up -d dc_protocol || sleep 3
-clear
 
 echo 'Migrate contracts'
 cd ../
-npm run migrate
+npm run migrate || sleep 3
+
+cd `dirname "$0"`/../_env/
+docker-compose down || sleep 3
--- src/Start.js
+++ src/Start.js
@@ -1,3 +1,4 @@
+const ncp   = require('ncp').ncp
 const path  = require('path')
 const spawn = require('child_process').spawn
 
@@ -6,9 +7,24 @@ module.exports = cmd => {
     ? 'dc_protocol'
     : ''
 
-  spawn(`docker-compose up -d ${protocol}`, {
+  const containerUp = spawn(`docker-compose up -d ${protocol}`, {
     stdio: 'inherit',
     cwd: path.resolve(__dirname, '../_env'),
     shell: true
+  })
+
+  containerUp.on('exit', code => {
+    if (code === 0) {
+      ncp(
+        path.resolve(__dirname, '../_env/protocol'),
+        path.resolve(process.cwd(), 'protocol'),
+        err => {
+          if (err) {
+            console.error(err)
+            process.exit()
+          }
+        }
+      )
+    }
   })
 }
