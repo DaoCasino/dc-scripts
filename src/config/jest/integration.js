9055a06ccfb76635a312276a19f8fcb076de4653
Ilya fanyShu <ilyashvy@gmail.com>
2018-09-11 16:07:31 +0300

    fix config for integration tests

--- src/config/jest/integration.js
+++ src/config/jest/integration.js
@@ -1,8 +1,8 @@
 const path = require('path')
 
 module.exports = {
-  rootDir: path.join(process.cwd(), './src/__tests__'),
-  roots: ['<rootDir>/integration'],
+  rootDir: path.join(process.cwd(), './src'),
+  roots: ['<rootDir>/__tests__'],
   reporters: [path.join(__dirname, '../../jest/reporter.js')],
   testPathIgnorePatterns: ['/node_modules/'],
   setupTestFrameworkScriptFile: path.join(__dirname, '../../jest/setup.js')
